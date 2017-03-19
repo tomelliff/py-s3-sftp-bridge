ea8df7bc7997d63ade159026e9e71db655c02566
Tom Elliff <tomelliff@gmail.com>
2017-03-19 09:04:01 +0000

    Add missing Lambda permission to allow scheduled event to fire

--- terraform/modules/s3-sftp-bridge_lambda/scheduled_event.tf
+++ terraform/modules/s3-sftp-bridge_lambda/scheduled_event.tf
@@ -1,5 +1,13 @@
 ###################################################################################################
 
+resource "aws_lambda_permission" "allow_scheduled_event" {
+  statement_id  = "AllowExecutionFromScheduledEvent"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.s3_sftp_bridge_lambda.arn}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.trigger_retry.arn}"
+}
+
 resource "aws_cloudwatch_event_rule" "trigger_retry" {
   name                = "${var.function_prefix}-${var.integration_name}"
   description         = "${var.retry_scheduled_event_description}"
